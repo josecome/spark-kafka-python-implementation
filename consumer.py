30354907aba69ae9e7a7a3a8b484e72094f4ad4f
Jose Jaime Come <josejaimecome@gmail.com>
2024-04-27 17:45:40 +0200

    Threads added to consumer.py

--- consumer.py
+++ consumer.py
@@ -1,23 +1,56 @@
 from kafka import KafkaConsumer
+from queue import Queue 
+from threading import Thread 
 import sqlalchemy
 import json
+from time import sleep
 
 engine = sqlalchemy.create_engine('mysql+pymysql://root:@localhost:3306/kafka')
 consumer = KafkaConsumer('my_favorite_topic', bootstrap_servers='localhost:9092',api_version=(0,1,0))
 
-def saving_data():
+def getting_data(out_q, c):
+    x = 0
     for message in consumer:
-        result = ''
         ms = message.value
-        json_data = json.loads(ms.decode('utf-8'))
+        out_q.put(ms.decode('utf-8'))
+        x += 1
+        c.put(x)
+    
+    x = 0
+
+def saving_data(in_q, c):
+    result = ''
+    stmt = f"INSERT INTO persons (firstname, lastname) VALUES "
+    sleep(10)
+    j = 0
+    while True:
+        j += 1    
+        data = in_q.get() 
+        json_data = json.loads(data)
         firstname = json_data['firstname']
         lastname = json_data['lastname']
-        stmt = f"INSERT INTO persons (firstname, lastname) VALUES ('{firstname}', '{lastname}')"
-        with engine.connect() as conn:
-            result = conn.execute(stmt)
-            #conn.commit()
-        # print(message)
-        # print(message.value)
+
+        stmt += f"('{firstname}', '{lastname}')"
+        cc = int(c.get())
+        if j < cc: stmt += ','
+        if cc: break
+
+    with engine.connect() as conn:
+        print(stmt)
+        result = conn.execute(stmt)
+
 
 if __name__ == "__main__":
-    saving_data()
+    # Create the shared queue and launch both threads
+    q = Queue()
+    c = Queue()
+    t1 = Thread(target = getting_data, args =(q, c,))
+    t2 = Thread(target = saving_data, args =(q, c,))
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+    # Block until all tasks are done.
+    # q.join()
